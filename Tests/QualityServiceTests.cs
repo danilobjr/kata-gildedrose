20c50f5eb51d1ae4875e3242214a8d2be367b543
Danilo Barros <danilobjr@gmail.com>
2016-11-07 11:10:09 -0200

    test added

--- Tests/QualityServiceTests.cs
+++ Tests/QualityServiceTests.cs
@@ -13,16 +13,34 @@ namespace Tests
         private readonly ItemBuilder SULFURAS_ITEM = ItemBuilder.AnItem().NamedSulfuras();
         private readonly ItemBuilder AGED_BRIE_ITEM = ItemBuilder.AnItem().NamedAgedBrie();
         private readonly ItemBuilder BACKSTAGE_ITEM = ItemBuilder.AnItem().NamedBackstage();
+        private readonly ItemBuilder NORMAL_ITEM = ItemBuilder.AnItem().AnyName();
         private readonly int DEFAULT_INITIAL_QUALITY = 10;
         private readonly int DEFAULT_INITIAL_SELLIN = 10;
         private readonly int MIN_QUALITY = 0;
         private readonly int MAX_QUALITY = 50;
 
         [Test]
+        public void UpdateQuality_SellInHasPassed_QualityDropsBy2()
+        {
+            var passedSellIn = -1;
+
+            var qualityService = AnQualityService()
+                .AddItem(NORMAL_ITEM
+                    .SellIn(passedSellIn)
+                    .Quality(DEFAULT_INITIAL_QUALITY)
+                    .Build())
+                .Build();
+
+            qualityService.UpdateQuality();
+
+            Assert.That(qualityService.Items.First().Quality, Is.EqualTo(DEFAULT_INITIAL_QUALITY - 2));
+        }
+
+        [Test]
         public void UpdateQuality_ItemHasMinQuality_QualityDoesntBecomeNegative()
         {
             var qualityService = AnQualityService()
-                .AddItem(SULFURAS_ITEM.Quality(MIN_QUALITY).Build())
+                .AddItem(NORMAL_ITEM.Quality(MIN_QUALITY).Build())
                 .Build();
 
             qualityService.UpdateQuality();
@@ -141,6 +159,23 @@ namespace Tests
             var noQuality = 0;
 
             Assert.That(qualityService.Items.First().Quality, Is.EqualTo(noQuality));
+        }
+
+        [Test]
+        public void UpdateQuality_SulfurasItemHasQualityOf80_QualityDoesntAlters()
+        {
+            var initialQuality = 80;
+
+            var qualityService = AnQualityService()
+                .AddItem(SULFURAS_ITEM
+                    .SellIn(DEFAULT_INITIAL_SELLIN)
+                    .Quality(initialQuality)
+                    .Build())
+                .Build();
+
+            qualityService.UpdateQuality();
+
+            Assert.That(qualityService.Items.First().Quality, Is.EqualTo(initialQuality));
         }
 
         private QualityServiceBuilder AnQualityService()
