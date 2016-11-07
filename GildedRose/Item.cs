ee69033bf69dd1562e6c9cfafe3bf32e40337f04
Danilo Barros <danilobjr@gmail.com>
2016-11-07 10:47:46 -0200

    Item class in its own file

---
+++ GildedRose/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public int SellIn { get; set; }
+        public int Quality { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            var anotherItem = obj as Item;
+
+            return this.Name == anotherItem.Name &&
+                this.Quality == anotherItem.Quality &&
+                this.SellIn == anotherItem.SellIn;
+        }
+    }
+}
--- GildedRose/QualityService.cs
+++ GildedRose/QualityService.cs
@@ -91,20 +91,4 @@ namespace Application
             }
         }
     }
-
-    public class Item
-    {
-        public string Name { get; set; }
-        public int SellIn { get; set; }
-        public int Quality { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            var anotherItem = obj as Item;
-
-            return this.Name == anotherItem.Name &&
-                this.Quality == anotherItem.Quality &&
-                this.SellIn == anotherItem.SellIn;
-        }
-    }
 }
